5d3988ad993862f21da6607b6215c7c8eedf4d21
Muhammad Anggi Kusuma <raden@DESKTOP-K1JPJT5.com>
2023-12-06 16:54:14 +0700

    membuat model genders dan roles

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,13 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('no_hp');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('photo');
+            $table->foreignId('id_gender');
+            $table->foreignId('id_role');
             $table->rememberToken();
             $table->timestamps();
         });
