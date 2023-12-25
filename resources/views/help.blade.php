b46c3208e3f4ed3b7d54842735a31ffd1b04ccac
SalmaSalsabi <salmasalsabila073@gmail.com>
2023-12-25 19:22:09 +0700

    ganti ukuran font

--- resources/views/help.blade.php
+++ resources/views/help.blade.php
@@ -48,17 +48,104 @@
             class="text-xl mt-8 Saya aman di NeedKost?font-semibold leading-none tracking-tight text-gray-900">
             Lupa Password?</h1>
     </div>
+
+    <div class="space-y-10 sm:space-y-14">
+        <h1
+            class="text-sm mt-8 font-semibold leading-none tracking-tight text-gray-900">
+            Selengkapnya</h1>
+    </div>
+
     <div class="space-y-10 sm:space-y-14">
         <h1
             class="text-xl mt-8 font-semibold leading-none tracking-tight text-gray-900">
             Panduan Keamanan</h1>
     </div>
+
     <div class="space-y-10 sm:space-y-14">
         <img class="h-8 w-8" src="https://i.pinimg.com/564x/9d/24/0c/9d240c635b387997e32ddb20a9e36f9f.jpg" alt="">
         <h1
             class="text-xl mt-8 Saya aman di NeedKost?font-semibold leading-none tracking-tight text-gray-900">
             Bagaimana Transaksi Saya Aman Di NeedKost?</h1>
     </div>
+    <div class="space-y-10 sm:space-y-14">
+        <h1
+            class="text-sm mt-8 font-semibold leading-none tracking-tight text-gray-900">
+            Selengkapnya</h1>
+    </div>
+
+    <div class="space-y-10 sm:space-y-14">
+        <h1
+            class="text-xl mt-8 font-semibold leading-none tracking-tight text-gray-900">
+            Produk dan Fitur untuk Penyewaan</h1>
+    </div>
+    <div class="space-y-10 sm:space-y-14">
+        <img class="h-8 w-8" src="https://i.pinimg.com/564x/3f/6c/b5/3f6cb5434714ce1c2da01a8d35466559.jpg" alt="">
+        <h1
+            class="text-xl mt-8 Saya aman di NeedKost?font-semibold leading-none tracking-tight text-gray-900">
+            Mengapa percakapan saya dengan pemilik kos di chat hilang?</h1>
+    </div>
+    <div class="space-y-10 sm:space-y-14">
+        <h1
+            class="text-sm mt-8 font-semibold leading-none tracking-tight text-gray-900">
+            Selengkapnya</h1>
+    </div>
+
+    <div class="space-y-10 sm:space-y-14">
+        <h1
+            class="text-xl mt-8 font-semibold leading-none tracking-tight text-gray-900">
+           Kebijakan NeedKost</h1>
+    </div>
+    <div class="space-y-10 sm:space-y-14">
+        <img class="h-8 w-8" src="https://i.pinimg.com/564x/9f/e1/f8/9fe1f86b8750210be476be0238c68a9e.jpg" alt="">
+        <h1
+            class="text-xl mt-8 Saya aman di NeedKost?font-semibold leading-none tracking-tight text-gray-900">
+            Kebijakan Privasi NeedKost</h1>
+    </div>
+    <div class="space-y-10 sm:space-y-14">
+        <h1
+            class="text-sm mt-8 font-semibold leading-none tracking-tight text-gray-900">
+            Selengkapnya</h1>
+    </div>
+
+    <div class="space-y-10 sm:space-y-14">
+        <h1
+            class="text-xl mt-8 font-semibold leading-none tracking-tight text-gray-900">
+           Akun Penyewa</h1>
+    </div>
+    <div class="space-y-10 sm:space-y-14">
+        <img class="h-8 w-8" src="https://i.pinimg.com/564x/9d/24/0c/9d240c635b387997e32ddb20a9e36f9f.jpg" alt="">
+        <h1
+            class="text-xl mt-8 Saya aman di NeedKost?font-semibold leading-none tracking-tight text-gray-900">
+            Saya lupa password akun penyewa, apa yang harus saya lakukan?</h1>
+    </div>
+    <div class="space-y-2 sm:space-y-3">
+        <h1
+            class="text-sm mt-8 font-semibold leading-none tracking-tight text-gray-900">
+            Selengkapnya</h1>
+    </div>
+
+    <div class="space-y-10 sm:space-y-14">
+        <h1
+            class="text-xl mt-8 font-semibold leading-none tracking-tight text-gray-900">
+           Syarat dan Ketentuan</h1>
+    </div>
+    <div class="space-y-10 sm:space-y-14">
+        <div class="w-1/2">
+        </div>
+        <img class="h-8 w-8" src="https://i.pinimg.com/564x/d3/dd/22/d3dd22232e921196b5eac4d3d3cf4fbc.jpg" alt="">
+        <h1
+            class="text-xl mt-8 Saya aman di NeedKost?font-semibold leading-none tracking-tight text-gray-900">
+            Syarat dan ketentuan umum</h1>
+    </div>
+    <div class="space-y-10 sm:space-y-10">
+        <h1
+            class="text-sm mt-8 font-semibold leading-none tracking-tight text-gray-900">
+            Selengkapnya</h1>
+    </div>
+</div>
+
+
+
 
 
     @include('layouts.footer')
