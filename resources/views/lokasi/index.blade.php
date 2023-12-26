b677a15ca240be76a2268c9ab827d1c699e981b4
02Anggi <radenanggikusuma@gmail.com>
2023-12-26 18:52:16 +0700

    mengerjakan get data di bagian lokasi

--- resources/views/lokasi/index.blade.php
+++ resources/views/lokasi/index.blade.php
@@ -25,31 +25,52 @@
                 </div>
                 <div
                     class="grid gap-y-12 sm:grid-cols-2 sm:gap-10 md:gap-x-4 md:grid-cols-2 lg:grid-cols-3 lg:gap-x-6 lg:gap-y-12">
-                    @for ($i = 0; $i < 3; $i++)
+                    @foreach ($kosts as $k)
                     <x-card>
                         <x-slot name="photo">
+                            @if ($k->photo()->exists())
+                            {{ asset('storage/' . $k->photo()->get()->first()->photo) }}
+                            @else
                             https://images.pexels.com/photos/439227/pexels-photo-439227.jpeg
+                            @endif
                         </x-slot>
                         <x-slot name="jenis">
-                            Campur
+                            {{ $k->gender->nama }}
                         </x-slot>
                         <x-slot name="nama">
-                            C03 Residence
+                            {{ $k->nama }}
                         </x-slot>
                         <x-slot name="alamat">
-                            Jl. Kapten Abdul Hamid No 3, Citarum, Bandung Wetan, Kota Bandung, Jawa Barat 40115
+                            {{ $k->alamat }}
                         </x-slot>
                         <x-slot name="fasilitas">
-                            AC, Wifi, Parkir Motor, Parkir Mobil, Dapur, Kamar Mandi Luar, Kamar Mandi Dalam
+                            @foreach ($k->fasilitas()->get() as $fasilitas)
+                                {{ $fasilitas->nama }}
+                                @if ($loop->last)
+                                @break
+                            @endif
+                            ,
+                            @endforeach
                         </x-slot>
                         <x-slot name="harga">
-                            Rp.500.000
+                            Rp {{ number_format($k->harga_per_bulan, 0, ',', '.') }}
                         </x-slot>
                         <x-slot name="url">
-
+                            <a href="{{ route('kost.detail', $k->id) }}"
+                                class="relative inline-flex items-center justify-center h-10 px-4 py-2 text-sm font-medium transition-colors border rounded-md lg: bg-emerald-200/80 dark:border-none dark:text-gray-200 dark:bg-emerald-600/90 ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-emerald -200-foreground hover:bg-emerald -200/80 dark:hover:bg-emerald-700/80 group">
+                                <span class="mr-6">Selengkapnya</span>
+                                <svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    class="absolute right-0 mr-4 !h-4 shrink-0 !stroke-2 duration-300 group-hover:mr-3">
+                                    <path
+                                    d="M15.2465 5.74628L19.3752 9.87494C20.5468 11.0465 20.5468 12.946 19.3752 14.1176L15.2465 18.2463M19.7465 11.9963H3.74655"
+                                    stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                                    stroke-linejoin="round"></path>
+                                    </svg>
+                            </a>
                         </x-slot>
                     </x-card>
-                    @endfor
+                    @endforeach
                 </div>
             </div>
         </div>
