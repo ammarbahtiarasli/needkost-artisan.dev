c0f398ab33ede1488fbb072396b5b803117d5e53
02Anggi <radenanggikusuma@gmail.com>
2023-12-31 16:00:23 +0700

    membuat fungsi info kost di dashboard

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -68,7 +68,7 @@
                         <p class="mt-1.5 text-sm text-muted-foreground dark:text-gray-300">Data semua kost</p>
                     </div>
                     <div class="container flex justify-end w-1/2">
-                        <form method="GET" action="{{ route('user.index') }}">
+                        <form method="GET" action="{{ route('dashboard') }}">
                             <label for="default-search"
                                 class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
                             <div class="relative items-end justify-end">
@@ -86,54 +86,45 @@
                         </form>
                     </div>
                 </div>
-                    <table class="w-full text-sm text-left text-gray-500 rtl:text-right dark:text-gray-400">
-                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                            <tr>
-                                <th scope="col" class="px-6 py-3">
-                                    Nama
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Jenis
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Alamat
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Harga
-                                </th>
-                                <th scope="col" class="px-6 py-3">
-                                    Status
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                                <tr
-                                    class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                    <th scope="row"
-                                        class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
-                                        <img class="w-10 h-10 rounded-full"
-                                            src="https://images.unsplash.com/photo-1486401899868-0e435ed85128?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
-                                            alt="Jese image">
-                                        <div class="ps-3">
-                                            <div class="text-base font-semibold">Kost Babeh</div>
-                                            <div class="font-normal text-gray-500"></div>
-                                        </div>
-                                    </th>
-                                    <td class="px-6 py-4">
-                                        Campuran
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        Jl. Babeh 
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        850.000
-                                    </td>
-                                    <td class="px-6 py-4">
-                                        Sewa
-                                    </td>
-                                </tr>
-                        </tbody>
-                    </table>
+                <table class="w-full text-sm text-left text-gray-500 rtl:text-right dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                Nama
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Jenis
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Alamat
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Harga
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($kosts as $kost)
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                            <th scope="row" class="flex items-center px-6 py-4 text-gray-900 whitespace-nowrap dark:text-white">
+                            <div class="">
+                                <div class="text-base font-semibold">{{ $kost->nama }}</div>
+                                <div class="font-normal text-gray-500"></div>
+                            </div>
+                            </th>
+                            <td class="px-6 py-4">
+                                {{ $kost->gender->nama }}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ $kost->alamat }}
+                            </td>
+                            <td class="px-6 py-4">
+                                Rp {{ number_format($kost->harga_per_bulan, 0, ',', '.') }}
+                            </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
             </div>
         </div>
     </div>
