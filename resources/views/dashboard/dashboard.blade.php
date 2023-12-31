b4aa6ee76376dc84354cb4b8d09c2ba3530ece56
02Anggi <radenanggikusuma@gmail.com>
2023-12-31 15:57:04 +0700

    fix kamar kost di dashboard

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -32,10 +32,9 @@
                 akun anda</p>
         </div>
         <div class="grid gap-5 sm:grid-cols-2 lg:grid-cols-3">
-            <div
-                class="p-6 text-gray-900 bg-white border rounded-lg shadow-sm dark:text-gray-200 dark:border-0 dark:bg-gray-800">
-                <h4 class="font-mono text-xl">{{ $kosts }}</h4><small class="block mt-4 text-sm text-muted-foreground">Kamar
-                    Kost</small>
+            <div class="p-6 text-gray-900 bg-white border rounded-lg shadow-sm dark:text-gray-200 dark:border-0 dark:bg-gray-800">
+                <h4 class="font-mono text-xl">{{ count($kosts) }}</h4>
+                <small class="block mt-4 text-sm text-muted-foreground">Kamar Kost</small>
                 <x-detail-long-button :href="route('kost.index')">Detail</x-detail-long-button>
             </div>
             <div
